9627cd7130c00885e75eaa738b48fb12c9fa434c
Abhishek Shukla <abhi2012mmmec@gmail.com>
2024-08-27 23:32:08 +0530

    Create InterfaceWithComposition.go
    This code explains how interface composition works and how methods got promoted in interface composition on user defined types

---
+++ InterfaceWithComposition.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+type Line struct {
+	Begin, End Point
+}
+
+type Path []Point
+
+func (l Line) Distance() float64 {
+	return math.Hypot(l.End.X-l.Begin.X, l.End.Y-l.Begin.Y)
+}
+
+func (p Path) Distance() (sum float64) {
+
+	for i := 1; i < len(p); i++ {
+		sum += Line{p[i-1], p[i]}.Distance()
+	}
+	return sum
+}
+
+type Distancer interface {
+	Distance() float64
+}
+
+func PrintDistance(d Distancer) {
+	fmt.Println(d.Distance())
+}
+
+func main() {
+
+	side := Line{Point{1, 1}, Point{5, 5}}
+
+	perimeter := Path{Point{1, 1}, Point{5, 5}, Point{5, 1}}
+
+	fmt.Println(side.Distance())
+	fmt.Println(perimeter.Distance())
+
+	PrintDistance(side)
+	PrintDistance(perimeter) // we can pass side,perimeter as distancer because they both inplements distance methods. Implicit conversion
+
+}
